047a33611eb2d0d72f61b513cf6de0d416983910
DmitryArhipenko <42931799+DmitryArhipenko@users.noreply.github.com>
2020-11-17 11:05:50 +0200

    deploy

---
+++ precache-manifest.fafbc5c25c7f320ae94e854c2f8e3053.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cafec335f69fe83aa6a",
+    "url": "/vue-test-task/css/app.f433b5e6.css"
+  },
+  {
+    "revision": "426ddabb26c0310a9e4a",
+    "url": "/vue-test-task/css/chunk-2727936c.ae237cc9.css"
+  },
+  {
+    "revision": "d1f561ec9bbde2f26e04",
+    "url": "/vue-test-task/css/chunk-4de0a176.bbbaaf9d.css"
+  },
+  {
+    "revision": "dd30368876ebecadaa3c",
+    "url": "/vue-test-task/css/chunk-vendors.24bf1742.css"
+  },
+  {
+    "revision": "829d1a55754482d5ce96fad71f5ea60c",
+    "url": "/vue-test-task/index.html"
+  },
+  {
+    "revision": "0cafec335f69fe83aa6a",
+    "url": "/vue-test-task/js/app.a5670db8.js"
+  },
+  {
+    "revision": "426ddabb26c0310a9e4a",
+    "url": "/vue-test-task/js/chunk-2727936c.76eef023.js"
+  },
+  {
+    "revision": "d1f561ec9bbde2f26e04",
+    "url": "/vue-test-task/js/chunk-4de0a176.193e9548.js"
+  },
+  {
+    "revision": "dd30368876ebecadaa3c",
+    "url": "/vue-test-task/js/chunk-vendors.2a68f7b4.js"
+  },
+  {
+    "revision": "d879e41c81cbf085015502b5fff80d51",
+    "url": "/vue-test-task/manifest.json"
+  }
+]);
